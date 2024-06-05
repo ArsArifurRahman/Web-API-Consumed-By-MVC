1052e571565c77c62ba069d95d746de54fbe5d38
Arifur Rahman <ars.ict.developer@gmail.com>
2024-06-06 00:52:55 +0600

    Country Repository Updated

--- Project/Server/Repository/Services/CountryRepository.cs
+++ Project/Server/Repository/Services/CountryRepository.cs
@@ -21,43 +21,21 @@ public class CountryRepository : ICountryRepository
 
     public async Task<Country> GetCountryAsync(int id)
     {
-        var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
-
-        if (country == null)
-        {
-            throw new InvalidOperationException("Country not found!");
-        }
-
+        var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id) ?? throw new InvalidOperationException("Country not found!");
         return country;
     }
 
     public async Task<Country> AddCountryAsync(Country country)
     {
-        if (country == null)
-        {
-            throw new ArgumentNullException(nameof(country));
-        }
-
+        ArgumentNullException.ThrowIfNull(country);
         await _context.Countries.AddAsync(country);
         await _context.SaveChangesAsync();
-
         return country;
     }
-
     public async Task EditCountryAsync(int id, Country country)
     {
-        if (country == null)
-        {
-            throw new ArgumentNullException(nameof(country));
-        }
-
-        var existingCountry = await GetCountryAsync(id);
-
-        if (existingCountry == null)
-        {
-            throw new KeyNotFoundException("The existing country with the given id was not found.");
-        }
-
+        ArgumentNullException.ThrowIfNull(country);
+        var existingCountry = await GetCountryAsync(id) ?? throw new KeyNotFoundException("The existing country with the given id was not found.");
         _context.Entry(existingCountry).CurrentValues.SetValues(country);
         await _context.SaveChangesAsync();
     }
@@ -76,20 +54,13 @@ public class CountryRepository : ICountryRepository
 
     public async Task<Country> GetCountryOfAnAuthorAsync(int authorId)
     {
-        var country = await _context.Authors.Where(x => x.Id == authorId).Select(y => y.Country).FirstOrDefaultAsync();
-
-        if (country == null)
-        {
-            throw new ArgumentException($"Country Author with ID {authorId} not found.");
-        }
-
+        var country = await _context.Authors.Where(x => x.Id == authorId).Select(y => y.Country).FirstOrDefaultAsync() ?? throw new ArgumentException($"Country Author with ID {authorId} not found.");
         return country;
     }
 
     public async Task<bool> IsCountryDuplicate(int id, string countryName)
     {
-        var country = await _context.Countries.Where(x => x.Name != null && x.Name.Trim().ToLower() == countryName.Trim().ToLower() && x.Id != id).FirstOrDefaultAsync();
-
+        var country = await _context.Countries.Where(x => x.Name != null && x.Name.Trim().Equals(countryName.Trim(), StringComparison.CurrentCultureIgnoreCase) && x.Id != id).FirstOrDefaultAsync();
         return country != null;
     }
 
--- Project/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Project/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d7023ff9768c9ca8f2c82f7fd264185c0630394")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82683a69203938c3117a38f470842b7c5100d38b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
